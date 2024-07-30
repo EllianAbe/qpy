14b0fb12d3c207a021b71aa175d70f790941bc60
EllianAbe <ellian.yabe@gmail.com>
2024-07-30 20:22:12 -0300

    add status, created_datetime, output to item_model

--- implementations/alchemy_queue/models/item_model.py
+++ implementations/alchemy_queue/models/item_model.py
@@ -1,13 +1,25 @@
 from ..utils import named_instance
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, JSON, ForeignKey
+from sqlalchemy import Column, Integer, JSON, ForeignKey, DateTime, Enum
 
 
 Base = named_instance.get('Base', declarative_base())
+
+
+class ItemStatus(Enum):
+    PENDING = 'pending'
+    PROCESSING = 'processing'
+    SUCCESS = 'success'
+    ERROR = 'error'
 
 
 class ItemModel(Base):
     __tablename__ = 'alchemy_item'
     id = Column(Integer, primary_key=True)
     queue_id = Column(Integer, ForeignKey('alchemy_queue.id'), nullable=False)
+    creation_datetime = Column(DateTime, default=DateTime())
+    status = Column(ItemStatus, default=ItemStatus.PENDING)
+    retry_count = Column(Integer, default=0)
+
     data = Column(JSON)
+    output_data = Column(JSON)

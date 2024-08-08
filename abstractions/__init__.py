a75e5dcc7449a7eaf86982ebb905a50127acf449
EllianAbe <ellian.yabe@gmail.com>
2024-08-07 22:06:48 -0300

    add item status on __init__

--- abstractions/__init__.py
+++ abstractions/__init__.py
@@ -1,5 +1,6 @@
 from .item import AbstractQueueItem
 from .qpy import AbstractQueue
+from .item_status import ItemStatus
 
 
-__all__ = ['AbstractQueue', 'AbstractQueueItem']
+__all__ = ['AbstractQueue', 'AbstractQueueItem', 'ItemStatus']

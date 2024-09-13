36ae5fdd38108b9cfb231f2192a1b8c43bde90e0
EllianAbe <ellian.yabe@gmail.com>
2024-09-12 21:57:26 -0300

    add exchange implementation with Exchange class

--- src/abstractions/__init__.py
+++
@@ -1,7 +0,0 @@
-from .item import AbstractQueueItem
-from .management.queue import Queue
-from .item_status import ItemStatus
-from . import errors
-
-
-__all__ = ['Queue', 'AbstractQueueItem', 'ItemStatus', 'errors']
--- src/abstractions/management/exchange.py
+++ src/abstractions/management/exchange.py
@@ -1,2 +1,17 @@
-class Exchange():
-    pass
+from src.abstractions.message import Message
+
+
+class Exchange:
+    def __init__(self, name, exchange_type):
+        self.name = name
+        self.exchange_type = exchange_type
+        self.bindings = {}
+
+    def bind(self, queue, routing_key):
+        self.bindings[routing_key] = queue
+
+    def route_message(self, message, routing_key):
+        if routing_key in self.bindings:
+            self.bindings[routing_key].enqueue(message)
+        else:
+            print(f"No binding for routing key: {routing_key}")

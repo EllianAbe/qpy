240ec9117200095d6ccbe8f938560899b21e8eaa
EllianAbe <ellian.yabe@gmail.com>
2024-09-12 22:01:39 -0300

    add __init__ imports modification

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+from .abstractions.management.broker import Broker
+from .abstractions.agents.producer import Producer
+from .abstractions.agents.consumer import Consumer
+
+__all__ = ["Broker", "Producer", "Consumer"]
